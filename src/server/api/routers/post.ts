ced92f4eeb00a67fd2925e85d83d604e4608ea88
Anil Beniwal <anil@beniwal.com>
2024-01-21 14:43:41 -0500

    add rate limiting

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,7 +1,9 @@
-import { User } from "@clerk/backend/dist/types/api";
+import type { User } from "@clerk/backend/dist/types/api";
 import { clerkClient } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
+import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
+import { Redis } from "@upstash/redis"; // see below for cloudflare and fastly adapters
 
 import { createTRPCRouter, privateProcedure, publicProcedure } from "~/server/api/trpc";
 
@@ -12,6 +14,13 @@ const filterUserForClient = (user: User) => {
     imageUrl: user.imageUrl
   };;
 }
+
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(3, "1 m"),
+  analytics: true,
+  prefix: "@upstash/ratelimit",
+});
 
 export const postRouter = createTRPCRouter({
 
@@ -51,6 +60,10 @@ export const postRouter = createTRPCRouter({
     .input(z.object({ content: z.string().emoji().min(1).max(280) }))
     .mutation(async ({ctx, input}) => {
       const authorId = ctx.userId;
+
+      const { success } = await ratelimit.limit(authorId);
+
+      if (!success) throw new TRPCError({code: "TOO_MANY_REQUESTS"});
 
       const post = await ctx.db.post.create({
         data: {
